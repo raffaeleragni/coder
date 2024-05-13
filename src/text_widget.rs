9da387d37b04c530d59451fc6b6cb3965bea62c9
Raffaele Ragni <raffaele.ragni@gmail.com>
2024-05-13 14:06:39 +0200

    coloring states

--- src/text_widget.rs
+++ src/text_widget.rs
@@ -1,6 +1,8 @@
 use ratatui::{
     buffer::Buffer,
-    layout::{Constraint, Layout, Rect},
+    layout::Rect,
+    style::Stylize,
+    text::{Line, Span, Text},
     widgets::{Paragraph, StatefulWidget, Widget},
 };
 
@@ -13,15 +15,23 @@ impl StatefulWidget for GameDisplay {
     type State = Game;
 
     fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
-        let l = Layout::default()
-            .direction(ratatui::layout::Direction::Vertical)
-            .constraints([Constraint::Min(1), Constraint::Min(1)])
-            .split(area);
-        Paragraph::new(state.target_text.clone())
-            .centered()
-            .render(l[0], buf);
-        Paragraph::new(state.typed_text.clone())
-            .centered()
-            .render(l[1], buf);
+        let text: Text = vec![Line::from(game_into_spans(state))].into();
+        Paragraph::new(text).centered().render(area, buf);
     }
+}
+
+fn game_into_spans(game: &Game) -> Vec<Span> {
+    let check = game.typed_text.as_str().chars().collect::<Vec<char>>();
+    let target = game.target_text.as_str();
+    let mut result = Vec::<Span>::new();
+    for (i, c) in target.chars().enumerate() {
+        if i >= check.len() {
+            result.push(Span::from(c.to_string()).dark_gray());
+        } else if check[i] == c {
+            result.push(Span::from(c.to_string()).bold().white());
+        } else {
+            result.push(Span::from(c.to_string()).bold().red());
+        }
+    }
+    result
 }
