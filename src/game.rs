e274b210895f5d2767d2547362faac367ed0d64b
Raffaele Ragni <raffaele.ragni@gmail.con>
2024-05-21 19:11:07 +0200

    keep span impl into ui

--- src/game.rs
+++ src/game.rs
@@ -1,5 +1,3 @@
-use ratatui::{style::Stylize, text::Span};
-
 #[derive(Debug)]
 pub struct Game {
     pub target_text: String,
@@ -24,26 +22,5 @@ impl Game {
 
     pub fn undo(&mut self) {
         self.typed_text.pop();
-    }
-}
-
-impl<'a> From<&Game> for Vec<Span<'a>> {
-    fn from(game: &Game) -> Self {
-        if game.done {
-            return vec![Span::from(game.target_text.clone()).bold().green()];
-        }
-        let check = game.typed_text.as_str().chars().collect::<Vec<char>>();
-        let target = game.target_text.as_str();
-        let mut result = Vec::<Span>::new();
-        for (i, c) in target.chars().enumerate() {
-            if i >= check.len() {
-                result.push(Span::from(c.to_string()).dark_gray());
-            } else if check[i] == c {
-                result.push(Span::from(c.to_string()).bold().white());
-            } else {
-                result.push(Span::from(c.to_string()).bold().red().underlined());
-            }
-        }
-        result
     }
 }
--- src/text_widget.rs
+++ src/text_widget.rs
@@ -1,6 +1,7 @@
 use ratatui::{
     buffer::Buffer,
     layout::Rect,
+    style::Stylize,
     text::{Line, Span, Text},
     widgets::{Paragraph, StatefulWidget, Widget},
 };
@@ -17,5 +18,26 @@ impl StatefulWidget for GameDisplay {
         let spans: Vec<Span> = (&*state).into();
         let text: Text = vec![Line::from(spans)].into();
         Paragraph::new(text).centered().render(area, buf);
+    }
+}
+
+impl<'a> From<&Game> for Vec<Span<'a>> {
+    fn from(game: &Game) -> Self {
+        if game.done {
+            return vec![Span::from(game.target_text.clone()).bold().green()];
+        }
+        let check = game.typed_text.as_str().chars().collect::<Vec<char>>();
+        let target = game.target_text.as_str();
+        let mut result = Vec::<Span>::new();
+        for (i, c) in target.chars().enumerate() {
+            if i >= check.len() {
+                result.push(Span::from(c.to_string()).dark_gray());
+            } else if check[i] == c {
+                result.push(Span::from(c.to_string()).bold().white());
+            } else {
+                result.push(Span::from(c.to_string()).bold().red().underlined());
+            }
+        }
+        result
     }
 }
