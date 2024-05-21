2b506eb4df972dd727ccbe9ee84ea2e9d2ce0fae
Raffaele Ragni <raffaele.ragni@gmail.con>
2024-05-21 19:09:57 +0200

    make into from

--- src/text_widget.rs
+++ src/text_widget.rs
@@ -1,7 +1,6 @@
 use ratatui::{
     buffer::Buffer,
     layout::Rect,
-    style::Stylize,
     text::{Line, Span, Text},
     widgets::{Paragraph, StatefulWidget, Widget},
 };
@@ -15,26 +14,8 @@ impl StatefulWidget for GameDisplay {
     type State = Game;
 
     fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
-        let text: Text = vec![Line::from(game_into_spans(state))].into();
+        let spans: Vec<Span> = (&*state).into();
+        let text: Text = vec![Line::from(spans)].into();
         Paragraph::new(text).centered().render(area, buf);
     }
-}
-
-fn game_into_spans(game: &Game) -> Vec<Span> {
-    if game.done {
-        return vec![Span::from(game.target_text.clone()).bold().green()];
-    }
-    let check = game.typed_text.as_str().chars().collect::<Vec<char>>();
-    let target = game.target_text.as_str();
-    let mut result = Vec::<Span>::new();
-    for (i, c) in target.chars().enumerate() {
-        if i >= check.len() {
-            result.push(Span::from(c.to_string()).dark_gray());
-        } else if check[i] == c {
-            result.push(Span::from(c.to_string()).bold().white());
-        } else {
-            result.push(Span::from(c.to_string()).bold().red().underlined());
-        }
-    }
-    result
 }
